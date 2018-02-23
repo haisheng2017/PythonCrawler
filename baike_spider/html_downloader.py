261228a9be0aeb2790564d88f9b279038e1021f3
haisheng2017 <895883668@qq.com>
2018-02-23 12:34:19 +0800

    python简易爬取静态网页爬虫

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,9 @@
+import urllib.request
+class HtmlDownloader(object):
+    def download(self, url):
+        if url is None:
+            return None
+        resp = urllib.request.urlopen(url)
+        if resp.getcode() != 200:
+            return None
+        return resp.read()
---
+++ test/testBS4.py
@@ -0,0 +1,40 @@
+import re
+
+from bs4 import BeautifulSoup
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup=BeautifulSoup(html_doc,'html.parser')
+# python3默认编码unicode 可以去掉utf-8
+
+print('获取所有的链接')
+
+links=soup.find_all('a');
+for link in links:
+    print(link.name,link['href'],link.get_text())
+
+print('获取lacie链接')
+link_node=soup.find('a',href='http://example.com/lacie')
+print(link_node.name, link_node['href'], link_node.get_text())
+
+print('正则匹配')
+# compile 加r 是确保匹配 / 而不用写//
+link_node=soup.find('a',href=re.compile(r"ill"))
+print(link_node.name, link_node['href'], link_node.get_text())
+
+print('获取p段落文字')
+# class是python关键字 所以用class_
+p_node=soup.find('p',class_="title")
+print(p_node.name, p_node.get_text())
